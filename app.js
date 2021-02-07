dffa1c5bf53b7a95ee7b748b5794e9966499a00a
zayedibnibrahim <zayed.ibn@gmail.com>
2021-02-07 20:40:30 +0600

    first call done

---
+++ app.js
@@ -0,0 +1,30 @@
+document.getElementById("search-btn").addEventListener('click', () => {
+    const searchInput = document.getElementById("search-input").value;
+    allMeal(searchInput);
+})
+
+const allMeal = searchMealName => {
+    const apiBase = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const url = `${apiBase}${searchMealName}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showAllMeals(data))
+}
+
+const showAllMeals = expectedMeal => {
+    const resultOfMeals = expectedMeal.meals;
+    resultOfMeals.forEach(element => {
+        const mealName = element.strMeal;
+        const mealImg = element.strMealThumb;
+        const mealCard = document.createElement("div");
+        mealCard.className = "card";
+        const allMealGrid = document.getElementById("food-items");
+        const mealContent = `
+        <img class="card-img-top" src="${mealImg}" alt="Meal Image">
+        <div class="card-body">
+        <h5 class="card-title text-center">${mealName}</h5>
+        </div>`;
+        mealCard.innerHTML = mealContent;
+        allMealGrid.appendChild(mealCard);
+    });
+}
