4798fa8649ef9d09ed43db91e83982afb8188767
zayedibnibrahim <zayed.ibn@gmail.com>
2021-02-07 22:46:15 +0600

    almost done

--- app.js
+++ app.js
@@ -10,15 +10,16 @@ const allMeal = searchMealName => {
         .then(res => res.json())
         .then(data => showAllMeals(data))
 }
-
+// Whole Grid
 const showAllMeals = expectedMeal => {
     const resultOfMeals = expectedMeal.meals;
     resultOfMeals.forEach(element => {
         const mealName = element.strMeal;
         const mealImg = element.strMealThumb;
+        const mealId = element.idMeal;
         const mealCard = document.createElement("div");
         mealCard.className = "card";
-        mealCard.setAttribute('onclick', `singleMeal('${mealName}')`);
+        mealCard.setAttribute('onclick', `singleMeal('${mealId}')`);
         const allMealGrid = document.getElementById("food-items");
         const mealContent = `
         <img class="card-img-top" src="${mealImg}" alt="Meal Image">
@@ -29,15 +30,41 @@ const showAllMeals = expectedMeal => {
         allMealGrid.appendChild(mealCard);
     });
 }
-
-const singleMeal = singleData => {
-    const apiBase = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-    const url = `${apiBase}${singleData}`;
+//Single Grid
+const singleMeal = singleMealId => {
+    const apiBase = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+    const url = `${apiBase}${singleMealId}`;
     fetch(url)
         .then(singleRes => singleRes.json())
         .then(singleData => showAllMealsData(singleData))
 }
 
 const showAllMealsData = eachMeal => {
+    const mealObject = eachMeal.meals[0];
+    const mealImg = mealObject.strMealThumb;
+    const mealName = mealObject.strMeal;
+    const mealIngredientsArray = ingredientsLi(mealObject);
+    for(let i=0; i< mealIngredientsArray.length; i++){
+        const allIngredients = mealIngredientsArray[i];
+        
+    }
     
+
+}
+//bunch of ingredients
+const ingredientsLi = ingredient => {
+    const ingredient1 = ingredient.strIngredient1;
+    const ingredient2 = ingredient.strIngredient2;
+    const ingredient3 = ingredient.strIngredient3;
+    const ingredient4 = ingredient.strIngredient4;
+    const ingredient5 = ingredient.strIngredient5;
+    const ingredient6 = ingredient.strIngredient6;
+    const ingredient7 = ingredient.strIngredient7;
+    const ingredient8 = ingredient.strIngredient8;
+    const ingredient9 = ingredient.strIngredient9;
+    const ingredient10 = ingredient.strIngredient10;
+
+    const ingredients =[ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient7, ingredient8, ingredient9, ingredient10];
+    
+    return ingredients;
 }
